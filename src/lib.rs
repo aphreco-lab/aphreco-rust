7a402899e7836532df6e8c0ae0f62a47fe897c72
sts-koyama <koyamasatoshi.3@gmail.com>
2022-01-12 21:53:15 +0900

    add examples/simulate.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 mod model;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+pub mod prelude {
+    pub use crate::model::SimModelTrait;
 }
