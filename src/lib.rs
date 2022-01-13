846aa87e9383ae5a1728f7bb95f7eb6a65580cbb
sts-koyama <koyamasatoshi.3@gmail.com>
2022-01-13 10:41:24 +0900

    remove rec model

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,8 @@
 mod model;
 
 pub mod prelude {
-    pub use crate::model::{
-        FixModelOptTrait, FlexModelOptTrait, OdeModelOptTrait, RecModelOptTrait,
-    };
-    pub use crate::model::{
-        FixModelSimTrait, FlexModelSimTrait, OdeModelSimTrait, RecModelSimTrait,
-    };
+    pub use crate::model::{FixModelOptTrait, FlexModelOptTrait, OdeModelOptTrait};
+    pub use crate::model::{FixModelSimTrait, FlexModelSimTrait, OdeModelSimTrait};
 
     pub use core::str::FromStr;
     pub use rust_decimal::Decimal;
--- src/model.rs
+++ src/model.rs
@@ -1,9 +1,7 @@
 mod fix;
 mod flex;
 mod ode;
-mod rec;
 
 pub use crate::model::fix::{FixModelOptTrait, FixModelSimTrait};
 pub use crate::model::flex::{FlexModelOptTrait, FlexModelSimTrait};
 pub use crate::model::ode::{OdeModelOptTrait, OdeModelSimTrait};
-pub use crate::model::rec::{RecModelOptTrait, RecModelSimTrait};
